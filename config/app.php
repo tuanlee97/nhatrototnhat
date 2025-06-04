f8279eca0943e034aac429ecfdf84f992d1b3b06
tuanlee97 <letuan30697@gmail.com>
2025-06-04 15:27:11 +0700

    up

--- config/app.php
+++ config/app.php
@@ -1,16 +1,16 @@
 <?php
 return array(
-    'YOUR_GOOGLE_CLIENT_ID' => 'YOUR_GOOGLE_CLIENT_ID',
+    'YOUR_GOOGLE_CLIENT_ID' => '353527097790-63llhal7fmkqapn7c5q1316p2l4ivaq2.apps.googleusercontent.com',
     'SECRET_KEY' => 'SECRET_KEY',
     // Cấu hình SMTP cho PHPMailer
     'SMTP_HOST' => 'smtp.gmail.com', // Server SMTP (ví dụ: Gmail)
     'SMTP_PORT' => 587, // Cổng SMTP
     'SMTP_ENCRYPTION' => 'tls', // Loại mã hóa (tls hoặc ssl)
-    'SMTP_USERNAME' => 'your-email@gmail.com', // Email gửi
-    'SMTP_PASSWORD' => 'your-app-password', // App Password của Gmail
-    'SMTP_FROM_EMAIL' => 'no-reply@yourdomain.com', // Email "From"
-    'SMTP_FROM_NAME' => 'Your App', // Tên "From"
+    'SMTP_USERNAME' => 'tuanlexor@gmail.com', // Email gửi
+    'SMTP_PASSWORD' => 'hkmc kmrf uiix hsma', // App Password của Gmail
+    'SMTP_FROM_EMAIL' => 'tuanlexor@gmail.com', // Email "From"
+    'SMTP_FROM_NAME' => 'Quản lý NTTN', // Tên "From"
     // URL frontend
-    'FRONTEND_URL' => 'http://your-frontend-domain', // URL của frontend
+    'FRONTEND_URL' => 'http://localhost/full', // URL của frontend
 );
 ?>
